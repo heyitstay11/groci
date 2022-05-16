bf1cb580af53e8cad96edc9ccc81b3a19bd1c230
Tayyab <heyitstay011@gmail.com>
2022-05-16 14:52:14 +0530

    layout

--- js/main.js
+++ js/main.js
@@ -1,17 +1,11 @@
-window.addEventListener('load', function(){
-    new Glider(document.querySelector('.glider.cats'), {
-        slidesToShow: 5,
-        draggable: true,
-        dots: '.dots',
-        arrows: {
-          prev: '.glider-prev',
-          next: '.glider-next'
-        }
-    })
-});
-
 const menuBtn = document.getElementById('menuBtn');
 
 menuBtn.addEventListener('click', () => {
   document.querySelector('.nav-list').classList.toggle('active')
 });
+
+const locationLabel = document.querySelector('label[for="location"]');
+
+locationLabel.addEventListener('click', () => {
+  locationLabel.nextElementSibling.classList.toggle('visually-hidden')
+})
