db871bc463d38e8bfa3b0a1cf63eea61442e63d2
Tayyab <heyitstay011@gmail.com>
2022-06-11 11:32:37 +0530

    minor changes

---
+++ server/routes/index.js
@@ -0,0 +1,5 @@
+export { default as authRouter } from './auth.js';
+export { default as cartRouter } from './cart.js';
+export { default as productRouter } from './product.js';
+export { default as orderRouter } from './order.js';
+export { default as paymentRouter } from './payment.js';
